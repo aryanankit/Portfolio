ab152c5813a5ae0b8e58ed8b4f778c6ebba01f65
aryanankit <a.arya8279@gmail.com>
2024-02-20 23:55:19 +0530

    Make some changes in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: 'jit',
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+        blue: '#2CBCE9',
+        red: '#DC4492',
+        yellow: '#FDCC49',
+        grey: '#ededed',
+        "deep-blue": '#010026',
+        "dark-grey": '#757575',
+        'opaque-black': 'rgba(0,0,0,0.35)'
+      }
+     },
   },
   plugins: [],
 }

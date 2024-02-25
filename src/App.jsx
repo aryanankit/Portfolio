c05070f2121d4a7b4dff31271345cb8d15e88ac4
aryanankit <a.arya8279@gmail.com>
2024-02-25 20:57:22 +0530

    completely built  the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./scenes/Navbar";
 const App = () => {
   const [selectedPage, setSelectedPage] = useState('home');
   const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)')
+  console.log(isAboveMediumScreens);
   return (
     <div className='app bg-deep-blue'>
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
